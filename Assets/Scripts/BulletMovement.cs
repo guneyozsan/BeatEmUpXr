cff522d89c718d9beeb2eff1666f7ea564d482e9
Guney Ozsan <guneyozsan@gmail.com>
2022-01-22 21:07:39 +0300

    Destroy bullet after max distance

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -3,9 +3,29 @@ using UnityEngine;
 public class BulletMovement : MonoBehaviour
 {
     [SerializeField] private float speedSeconds;
+    [SerializeField] private float maxDistance;
     
+    private Vector3 initialPosition;
+
+    private void Start() =>
+        initialPosition = transform.position;
+
     private void Update()
     {
-        transform.position += Time.deltaTime * speedSeconds * Vector3.forward;
+        Translate();
+        DestroyIfMaxDistance();
+
+        void Translate() =>
+            transform.position += Time.deltaTime * speedSeconds * Vector3.forward;
+
+        void DestroyIfMaxDistance()
+        {
+            Vector3 deltaPosition = transform.position - initialPosition;
+
+            if (deltaPosition.magnitude > maxDistance)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
