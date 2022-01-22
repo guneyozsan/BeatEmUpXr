28487910b7ddb1028ddda15bce71472a5633fdee
Guney Ozsan <guneyozsan@gmail.com>
2022-01-22 13:32:31 +0300

    Implement debug player input

---
+++ Assets/Scripts/TurretManager.cs
@@ -0,0 +1,130 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretManager : MonoBehaviour
+{
+    private void OnFire0()
+    {
+        Debug.Log("Fire 0");
+    }
+
+    private void OnFire1()
+    {
+        Debug.Log("Fire 1");
+    }
+    
+    private void OnFire2()
+    {
+        Debug.Log("Fire 2");
+    }
+    
+    private void OnFire3()
+    {
+        Debug.Log("Fire 3");
+    }
+    
+    private void OnFire4()
+    {
+        Debug.Log("Fire 4");
+    }
+    
+    private void OnFire5()
+    {
+        Debug.Log("Fire 5");
+    }
+    
+    private void OnFire6()
+    {
+        Debug.Log("Fire 6");
+    }
+    
+    private void OnFire7()
+    {
+        Debug.Log("Fire 7");
+    }
+    
+    private void OnFire8()
+    {
+        Debug.Log("Fire 8");
+    }
+    
+    private void OnFire9()
+    {
+        Debug.Log("Fire 9");
+    }
+    
+    private void OnFire10()
+    {
+        Debug.Log("Fire 10");
+    }
+    
+    private void OnFire11()
+    {
+        Debug.Log("Fire 11");
+    }
+    
+    private void OnFire12()
+    {
+        Debug.Log("Fire 12");
+    }
+    
+    private void OnFire13()
+    {
+        Debug.Log("Fire 13");
+    }
+    
+    private void OnFire14()
+    {
+        Debug.Log("Fire 14");
+    }
+    
+    private void OnFire15()
+    {
+        Debug.Log("Fire 15");
+    }
+    
+    private void OnFire16()
+    {
+        Debug.Log("Fire 16");
+    }
+    
+    private void OnFire17()
+    {
+        Debug.Log("Fire 17");
+    }
+    
+    private void OnFire18()
+    {
+        Debug.Log("Fire 18");
+    }
+    
+    private void OnFire19()
+    {
+        Debug.Log("Fire 19");
+    }
+    
+    private void OnFire20()
+    {
+        Debug.Log("Fire 20");
+    }
+    
+    private void OnFire21()
+    {
+        Debug.Log("Fire 21");
+    }
+    
+    private void OnFire22()
+    {
+        Debug.Log("Fire 22");
+    }
+    
+    private void OnFire23()
+    {
+        Debug.Log("Fire 23");
+    }
+    
+    private void OnFire24()
+    {
+        Debug.Log("Fire 24");
+    }
+}
